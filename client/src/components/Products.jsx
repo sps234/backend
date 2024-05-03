c9b8af09db91714aa89ab1068f81c4e42cf95818
sam <saumyapratap@lpu.in>
2024-05-03 23:57:52 +0530

    refactor: Update API base URL to use relative path

--- client/src/components/Products.jsx
+++ client/src/components/Products.jsx
@@ -20,8 +20,8 @@ const Products = ({ cat, filters, sort }) => {
       try {
         const res = await axios.get(
           cat
-            ? `http://localhost:5000/backend/products?category=${cat}`
-            : "http://localhost:5000/backend/products"
+            ? `/backend/products?category=${cat}`
+            : "/backend/products"
         );
         setProducts(res.data);
       } catch (err) { }
